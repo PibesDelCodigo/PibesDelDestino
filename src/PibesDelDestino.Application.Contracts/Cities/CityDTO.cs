dba3b8fbb4f8e66271eedd8101d203c6d92e8ae0
matufle <147943942+matufle@users.noreply.github.com>
2025-10-14 17:25:15 -0300

    Creacion Cities y CityServices

---
+++ src/PibesDelDestino.Application.Contracts/Cities/CityDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PibesDelDestino.Cities
+{
+    public class CityDto
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; } // Representa el estado o provincia
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
+
---
+++ src/PibesDelDestino.Application.Contracts/Cities/CityResultDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PibesDelDestino.Cities
+{
+    public class CityResultDto
+    {
+        public List<CityDto> Cities { get; set; }
+    }
+}
