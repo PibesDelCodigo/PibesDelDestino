cf8f6b650a2b569a1013052872600f4bc2223dba
JulianC <juliancarmona743@gmail.com>
2026-02-08 00:33:50 -0300

    detalles

---
+++ src/PibesDelDestino.Application.Contracts/Metrics/DashboardDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PibesDelDestino.Metrics
+{
+    public class DashboardDto
+    {
+        // Estadísticas Técnicas
+        public int TotalApiCalls { get; set; }
+        public double SuccessRate { get; set; }
+        public double AvgResponseTime { get; set; }
+
+        // Estadísticas de Negocio (Top Búsquedas)
+        public Dictionary<string, int> TopSearches { get; set; }
+    }
+}
---
+++ src/PibesDelDestino.Domain/Metrics/SearchHistory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace PibesDelDestino.Metrics
+{
+    public class SearchHistory : CreationAuditedEntity<Guid>
+    {
+        public string Term { get; set; }      // Ej: "Paris", "Playa"
+        public int ResultCount { get; set; }  // Cuántos resultados devolvió la API
+
+        protected SearchHistory() { }
+
+        public SearchHistory(Guid id, string term, int resultCount) : base(id)
+        {
+            Term = term;
+            ResultCount = resultCount;
+        }
+    }
+}
