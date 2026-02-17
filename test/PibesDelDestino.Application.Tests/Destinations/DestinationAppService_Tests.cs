fae993987b25a2930acedb6f4ce4029eefeaa927
JulianC <juliancarmona743@gmail.com>
2026-02-16 21:28:27 -0300

    cambios

--- test/PibesDelDestino.Application.Tests/Destinations/DestinationAppService_Tests.cs
+++ test/PibesDelDestino.Application.Tests/Destinations/DestinationAppService_Tests.cs
@@ -71,7 +71,9 @@ namespace PibesDelDestino.Destinations
                 _destinationRepoMock,
                 _identityUserRepoMock,
                 _emailSenderMock,
-                _loggerMock
+                _loggerMock,
+                _emailSenderMock, // <-- Segundo IEmailSender requerido por el constructor
+                _loggerMock       // <-- Segundo ILogger<NotificationManager> requerido por el constructor
             );
 
             // 5. Instanciar el Servicio (Usando el nuevo constructor)
