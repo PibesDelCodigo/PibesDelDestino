73923ade2edade6a16fae09e3487f971def8a7d1
Matias Kloster <100063391+KlosterMatias@users.noreply.github.com>
2026-02-18 17:45:36 -0300

    Acomodando-tests

--- test/PibesDelDestino.EntityFrameworkCore.Tests/EntityFrameworkCore/Aplicattion/Destinations/EfCoreDestinationAppService_Tests.cs
+++ test/PibesDelDestino.EntityFrameworkCore.Tests/EntityFrameworkCore/Aplicattion/Destinations/EfCoreDestinationAppService_Tests.cs
@@ -1,13 +1,11 @@
 ﻿using PibesDelDestino.Destinations;
-using PibesDelDestino.EntityFrameworkCore;
 using Xunit;
 
-namespace PibesDelDestino.EntityFrameworkCore.Destinations
+namespace PibesDelDestino.EntityFrameworkCore.Applications.Destinations
 {
-    // Esta es la clase que XUnit va a encontrar. 
-    // Al heredar de la anterior, ejecuta todos sus tests.
+    [Collection(PibesDelDestinoTestConsts.CollectionDefinitionName)]
     public class EfCoreDestinationAppService_Tests : DestinationAppService_Tests<PibesDelDestinoEntityFrameworkCoreTestModule>
     {
-        // No hace falta poner código acá, hereda todo de la clase base genérica.
+
     }
 }
